63ecd565268f1cd3a272265a574a2a5e869feb46
Basti Destruction <basti@dest.cc>
2018-03-01 09:03:12 +0000

    change style

--- src/TreeViewItem.vue
+++ src/TreeViewItem.vue
@@ -1,122 +1,126 @@
-<template>
-  <div class="tree-view-item">
-    <div v-if="isObject(data)" class="tree-view-item-leaf">
-      <div class="tree-view-item-node" @click.stop="toggleOpen()" >
-        <span :class="{opened: isOpen()}" class="tree-view-item-key tree-view-item-key-with-chevron">{{getKey(data)}}</span>
-        <span class="tree-view-item-hint" v-show="!isOpen() && data.children.length === 1">{{data.children.length}} property</span>
-        <span class="tree-view-item-hint" v-show="!isOpen() && data.children.length !== 1">{{data.children.length}} properties</span>
-      </div>
-      <tree-view-item :key="getKey(child)" :max-depth="maxDepth" :current-depth="currentDepth+1" v-show="isOpen()" v-for="child in data.children" :data="child" :modifiable="modifiable" @change-data="onChangeData"></tree-view-item>
-    </div>
-    <div v-if="isArray(data)" class="tree-view-item-leaf">
-      <div class="tree-view-item-node" @click.stop="toggleOpen()">
-        <span :class="{opened: isOpen()}"  class="tree-view-item-key tree-view-item-key-with-chevron">{{getKey(data)}}</span>
-        <span class="tree-view-item-hint" v-show="!isOpen() && data.children.length === 1">{{data.children.length}} item</span>
-        <span class="tree-view-item-hint" v-show="!isOpen() && data.children.length !== 1">{{data.children.length}} items</span>
-      </div>
-      <tree-view-item :key="getKey(child)" :max-depth="maxDepth" :current-depth="currentDepth+1" v-show="isOpen()" v-for="child in data.children" :data="child" :modifiable="modifiable" @change-data="onChangeData"></tree-view-item>
-    </div>
-    <tree-view-item-value v-if="isValue(data)" class="tree-view-item-leaf" :key-string="getKey(data)" :data="data.value" :modifiable="modifiable" @change-data="onChangeData">
-    </tree-view-item-value>
-  </div>
+<template lang="pug">
+.tree-view-item
+  .tree-view-item-leaf(v-if='isObject(data)')
+    .tree-view-item-node(@click.stop='toggleOpen()')
+      span.tree-view-item-key.tree-view-item-key-with-chevron(:class='{opened: isOpen()}') {{getKey(data)}}
+      span.tree-view-item-hint(v-show='!isOpen() && data.children.length === 1') {{data.children.length}} property
+      span.tree-view-item-hint(v-show='!isOpen() && data.children.length !== 1') {{data.children.length}} properties
+    tree-view-item(:key='getKey(child)', :max-depth='maxDepth', :current-depth='currentDepth+1', v-show='isOpen()', v-for='child in data.children', :data='child', :modifiable='modifiable', @change-data='onChangeData')
+  .tree-view-item-leaf(v-if='isArray(data)')
+    .tree-view-item-node(@click.stop='toggleOpen()')
+      span.tree-view-item-key.tree-view-item-key-with-chevron(:class='{opened: isOpen()}') {{getKey(data)}}
+      span.tree-view-item-hint(v-show='!isOpen() && data.children.length === 1') {{data.children.length}} item
+      span.tree-view-item-hint(v-show='!isOpen() && data.children.length !== 1') {{data.children.length}} items
+    tree-view-item(:key='getKey(child)', :max-depth='maxDepth', :current-depth='currentDepth+1', v-show='isOpen()', v-for='child in data.children', :data='child', :modifiable='modifiable', @change-data='onChangeData')
+  tree-view-item-value.tree-view-item-leaf(v-if='isValue(data)', :key-string='getKey(data)', :data='data.value', :modifiable='modifiable', @change-data='onChangeData')
 </template>
 
 <script>
-  import _ from 'lodash'
-  import TreeViewItemValue from './TreeViewItemValue.vue'
+import _ from 'lodash';
+import TreeViewItemValue from './TreeViewItemValue.vue';
 
-  export default {
-    components: {
-      TreeViewItemValue
+export default {
+  name: 'TreeViewItem',
+  components: {
+    TreeViewItemValue
+  },
+  props: {
+    'data': {
+      type: [Object, Array, String, Number, Boolean, Symbol],
+      default: () => {return {};}
     },
-  	name: "tree-view-item",
-    props: ["data", "max-depth", "current-depth", "modifiable"],
-    data: function(){
-    	return {
-      	open: this.currentDepth < this.maxDepth
+    'max-depth': {
+      type: Number,
+      default: 3
+    },
+    'current-depth': {
+      type: Number,
+      default: 0
+    },
+    'modifiable': {
+      type: Boolean,
+      default: false
+    },
+  },
+  data: function(){
+    return {
+      open: this.currentDepth < this.maxDepth
+    };
+  },
+  methods: {
+    isOpen: function(){
+      return this.open;
+    },
+    toggleOpen:function(){
+      this.open = !this.open;
+    },
+    isObject: function(value){
+      return value.type === 'object';
+    },
+    isArray: function(value){
+      return value.type === 'array';
+    },
+    isValue: function(value){
+      return value.type === 'value';
+    },
+    getKey: function(value){
+      if (_.isInteger(value.key)) {
+        return value.key+':';
+      } else {
+        return '"' + value.key + '":';
       }
     },
-    methods: {
-      isOpen: function(){
-      	return this.open;
-      },
-      toggleOpen:function(){
-      	this.open = !this.open;
-      },
-    	isObject: function(value){
-      	return value.type === 'object';
-      },
-    	isArray: function(value){
-      	return value.type === 'array';
-      },
-      isValue: function(value){
-      	return value.type === 'value';
-      },
-      getKey: function(value){
-      	if (_.isInteger(value.key)) {
-        	return value.key+":";
-        } else {
-  	      return "\""+ value.key + "\":";
-        }
-      },
-      isRootObject: function(value = this.data){
-      	return value.isRoot;
-      },
-      onChangeData: function(path, value) {
-        path = _.concat(this.data.key, path)
-        this.$emit('change-data', path, value)
-      }
+    isRootObject: function(value = this.data){
+      return value.isRoot;
+    },
+    onChangeData: function(path, value) {
+      path = _.concat(this.data.key, path);
+      this.$emit('change-data', path, value);
     }
-  };
+  }
+};
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
 
-.tree-view-item {
-  font-family: monaco, monospace;
-  font-size: 14px;
-  margin-left: 18px;
-}
+.tree-view-item
+  font-family: monaco, monospace
+  font-size: 14px
+  margin-left: 18px
 
-.tree-view-item-node {
-  cursor: pointer;
-  position: relative;
-  white-space: nowrap;
-}
+.tree-view-item-node 
+  cursor: pointer
+  position: relative
+  white-space: nowrap
 
-.tree-view-item-leaf {
-  white-space: nowrap;
-}
+.tree-view-item-leaf 
+  white-space: nowrap
 
-.tree-view-item-key {
-  font-weight: bold;
-}
+.tree-view-item-key 
+  font-weight: bold
 
-.tree-view-item-key-with-chevron {
-  padding-left: 14px;
-}
+.tree-view-item-key-with-chevron 
+  padding-left: 14px
 
 
-.tree-view-item-key-with-chevron.opened::before {
-    top:4px;
-    transform: rotate(90deg);
-    -webkit-transform: rotate(90deg);
-}
+.tree-view-item-key-with-chevron.opened::before 
+  top: 4px
+  transform: rotate(90deg)
+  -webkit-transform: rotate(90deg)
 
-.tree-view-item-key-with-chevron::before {
-    color: #444;
-    content: '\25b6';
-    font-size: 10px;
-    left: 1px;
-    position: absolute;
-    top: 3px;
-    transition: -webkit-transform .1s ease;
-    transition: transform .1s ease;
-    transition: transform .1s ease, -webkit-transform .1s ease;
-    -webkit-transition: -webkit-transform .1s ease;
-}
+.tree-view-item-key-with-chevron::before 
+  color: #444
+  content: '\25b6'
+  font-size: 10px
+  left: 1px
+  position: absolute
+  top: 3px
+  transition: -webkit-transform .1s ease
+  transition: transform .1s ease
+  transition: transform .1s ease, -webkit-transform .1s ease
+  -webkit-transition: -webkit-transform .1s ease
 
-.tree-view-item-hint {
+.tree-view-item-hint 
   color: #ccc
-}
+
 </style>
--- src/TreeViewItemValue.vue
+++ src/TreeViewItemValue.vue
@@ -1,91 +1,102 @@
-<template>
-  <div>
-    <span class="tree-view-item-key">{{keyString}}</span>
-    <input v-if="modifiable" class="tree-view-item-value" :class="getValueType(data)" v-model="valueString" @keyup.enter="onUpdateData" @blur="onUpdateData">
-    <span v-else class="tree-view-item-value" :class="getValueType(data)">{{ valueFormed }}</span>
-    <span v-show="error">{{ error }}</span>
-  </div>
+<template lang="pug">
+div
+  span.tree-view-item-key {{keyString}}
+  input.tree-view-item-value(v-if='modifiable', :class='getValueType(data)', v-model='valueString', @keyup.enter='onUpdateData', @blur='onUpdateData')
+  span.tree-view-item-value(v-else='', :class='getValueType(data)') {{ valueFormed }}
+  span(v-show='error') {{ error }}
 </template>
 
-
 <script>
-import _ from 'lodash'
+import _ from 'lodash';
 
 export default {
-	name: 'tree-view-item',
-  props: ['data', 'modifiable', 'key-string'],
+  name: 'TreeViewItem',
+  props: {
+    'data': {
+      type: [Object, Array, String, Number, Boolean, Symbol],
+      default: () => {return {};}
+    },
+    'modifiable': {
+      type: Boolean,
+      default: false
+    },
+    'key-string': {
+      type: String,
+      default: null
+    }
+  },
   data: function(){
-  	return {
-  		valueString: this.data && this.data.toString(),
+    return {
+      valueString: this.data && this.data.toString(),
       error: false,
-  	}
+    };
   },
   computed: {
     valueFormed: function () {
-      return this.getValue(this.data)
+      return this.getValue(this.data);
     }
   },
   methods: {
     onUpdateData: function() {
       try {
-        let v = this.typedValue(this.valueString)
-        this.error = false
-        this.$emit('change-data', [], v)
+        let v = this.typedValue(this.valueString);
+        this.error = false;
+        this.$emit('change-data', [], v);
       }
       catch (err) {
-        this.error = err
+        this.error = err;
       }
     },
     typedValue: function(v) {
-      if (v == '') { throw 'empty' }
+      if (v == '') { throw 'empty'; }
 
-      let dataType = this.getValueType(this.data, '')
+      let dataType = this.getValueType(this.data, '');
 
       switch (dataType) {
-        case 'number':
-          if (_.isNaN(_.toNumber(v))) {
-            throw 'only number'
-          }
-          return _.toNumber(v)
-        case 'boolean':
-          if (v.toLowerCase() === 'true') { return true }
-          if (v.toLowerCase() === 'false') { return false }
-          throw 'true or false'
-        case 'string':
-        default:
-          return v
+      case 'number':
+        if (_.isNaN(_.toNumber(v))) {
+          throw 'only number';
+        }
+        return _.toNumber(v);
+      case 'boolean':
+        if (v.toLowerCase() === 'true') { return true; }
+        if (v.toLowerCase() === 'false') { return false; }
+        throw 'true or false';
+      case 'string':
+      default:
+        return v;
       }
     },
     getValue: function(value){
       if (_.isNumber(value)) {
-        return value
+        return value;
       }
       if (_.isNull(value)) {
-        return "null"
+        return 'null';
       }
       if (_.isString(value)) {
-        return "\""+value+"\"";
+        return '"' + value + '"';
       }
       return value;
     },
-    getValueType: function(value, prefix="tree-view-item-value-"){
+    getValueType: function(value, prefix='tree-view-item-value-'){
       if (_.isNumber(value)) {
-        return prefix + "number"
+        return prefix + 'number';
       }
       if (_.isFunction(value)) {
-        return prefix + "function"
+        return prefix + 'function';
       }
       if (_.isBoolean(value)) {
-        return prefix + "boolean"
+        return prefix + 'boolean';
       }
       if (_.isNull(value)) {
-        return prefix + "null"
+        return prefix + 'null';
       }
       if (_.isString(value)) {
-        return prefix + "string";
+        return prefix + 'string';
       }
-      return prefix + "unknown";
+      return prefix + 'unknown';
     }
   }
-}
+};
 </script>
