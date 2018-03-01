fe912730ad63b705238874daa18b66ab8291a0a3
Basti Destruction <basti@dest.cc>
2018-03-01 08:39:40 +0000

    webpack prod build

--- dev/App.vue
+++ dev/App.vue
@@ -1,12 +1,15 @@
 <template lang="pug">
 .app
   h1 Dev test
+  tree-view(:data='data')
 </template>
 
 <script>
 import Vue from 'vue';
 import TreeView from '../src';
 Vue.use(TreeView);
+
+import testJson from './res/test-json';
 
 export default {
   name: 'App',
@@ -15,7 +18,7 @@ export default {
   },
   data() {
     return {
-
+      data: testJson
     };
   },
   computed: {
